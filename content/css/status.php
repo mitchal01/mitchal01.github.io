a878726f31fe5b0557c1c4f0b6c37e01d2c405d0
mitchal01 <wilhelm.micah.d.civ@gmail.com>
2021-09-12 20:10:43 -1000

    Update status.php

--- content/css/status.php
+++ content/css/status.php
@@ -1,18 +1,15 @@
 <?php // Need Any Help Read ReadMe.txt Or Message us at: https://discord.gg/qqVBb53 //
-/*———————–[ SETTINGS ]——————————*/
-$server_settings[‘ip’] = “69.174.154.177”; // localhost for local servers / IP or domain name for VDS/VPS
-$server_settings[‘port’] = “30120”; // basically 30120
-$server_settings[‘max_slots’] = “64”; // maximum slots. By default 24
-$url1=$_SERVER[‘REQUEST_URI’]; // Dont Change Any Lines Below
-header(“Refresh: 5; URL=$url1”);
-/*—————————————————————-*/
-
-$content = json_decode(file_get_contents(“http://”.$server_settings[‘ip’].”:”.$server_settings[‘port’].”/info.json”), true);
+/*-----------------------[ SETTINGS ]------------------------------*/
+$server_settings['ip'] = "69.174.154.177"; // localhost for local servers / IP or domain name for VDS/VPS
+$server_settings['port'] = "30120"; // basically 30120
+$url1=$_SERVER[‘REQUEST_URI’];    // Dont Change Any Lines Below
+header("Refresh: 5; URL=$url1");
+/*----------------------------------------------------------------*/
+$content = json_decode(@file_get_contents("http://".$server_settings['ip'].":".$server_settings['port']."/info.json"), true);
 if($content):
-$gta5_players = file_get_contents(“http://”.$server_settings[‘ip’].”:”.$server_settings[‘port’].”/players.json”);
-$content = json_decode($gta5_players, true);
-$pl_count = count($content);
-$SRV_STATUS = “<font style=’color: green;’>Online</font>”;
-print ” $pl_count / $server_settings[max_slots] “;
+    $SRV_STATUS = "Online";
+else:
+	$SRV_STATUS = "Offline";
 endif;
+print " $SRV_STATUS";
 ?>
